0661ec11df920662485b3688ab8a891417032bab
Josep IA <josep.itarte@gmail.com>
2013-12-02 20:55:55 +0100

    update release

---
+++ app/models/Worktype.java
@@ -0,0 +1,37 @@
+package models;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import play.data.validation.Constraints;
+import play.db.ebean.Model;
+
+@Entity
+public class Worktype extends Model{
+	
+	private static final long serialVersionUID = 1L;
+
+	@Id
+    public Long id;
+    
+    @Constraints.Required
+    public String name;
+    
+    /**
+     * Generic query helper for entity Company with id Long
+     */
+    public static Model.Finder<Long,Worktype> find = new Model.Finder<Long,Worktype>(Long.class, Worktype.class);
+
+    public static Map<String,String> options() {
+        LinkedHashMap<String,String> options = new LinkedHashMap<String,String>();
+        for(Worktype c: Worktype.find.orderBy("name").findList()) {
+            options.put(c.id.toString(), c.name);
+        }
+        return options;
+    }
+
+	
+}
